c0645406a06929187f187ec493d2f2429fb7ce6a
Lyes SADAT <lyessadat@stagiaire.ief2i.fr>
2022-12-02 10:02:03 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+using WebApplication1.ViewModels.Home;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var VM = new IndexVM();
+            return View(VM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Infos()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/Home/Index.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.ViewModels.Home;
+
+public class IndexVM
+{
+    public string Title { get; set; }
+
+    public IndexVM()
+    { 
+        this.Title = "Page d'accueil"; 
+    }
+}
